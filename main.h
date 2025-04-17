0c8094886fa46c18916e1e0d59532e5d72dc5f98
BenoitMain <benoitmaingon@gmail.com>
2025-04-17 14:31:10 +0200

    Test Branch with some prog

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
+typedef struct command
+{
+	char " "
+};
+
+
+#endif
