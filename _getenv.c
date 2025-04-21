5c10f9f137ab9027eebdde58531d336464e9bb7d
Annece2935 <anne-cecile.colleter@live.fr>
2025-04-21 16:43:46 +0200

    update avec le PATH tache 3.
    Co-authored-by: BenoitMain <benoitmaingon@gmail.com>

---
+++ _getenv.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include "shell.h"
+/**
+ * _getenv - Retrieves the value of an environment variable
+ * @path: Name of the environment variable to find (e.g., "PATH")
+ *
+ * Return: A pointer to the value of the environment variable,
+ * or NULL if the variable is not found.
+ */
+char *_getenv(const char *path)
+{
+	size_t len;
+	int i;
+
+	len = strlen(path);
+
+	for (i = 0; environ[i] != NULL; i++)
+	{
+		if (strncmp(environ[i], path, len) == 0 && environ[i][len] == '=')
+		{
+			return (environ[i] + len + 1);
+		}
+	}
+	return (NULL);
+}
