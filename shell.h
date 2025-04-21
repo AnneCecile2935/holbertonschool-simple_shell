557eed7768dce42df8977531b42a41ce039b183e
Annece2935 <anne-cecile.colleter@live.fr>
2025-04-21 12:24:55 +0200

    update prompt without getenv et message d'erreur suite à la sortie avec Ctrl C Co-authored-by: BenoitMain <benoitmaingon@gmail.com>

--- shell.h
+++ shell.h
@@ -9,7 +9,6 @@ extern char **environ;
 
 int interactive_shell(void);
 void print_prompt(void);
-ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
 char *read_command(void);
 void execute_command(char **args, char **envp);
 char **tokenize_string(char *str);
